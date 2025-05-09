435707bd8d6e5d2891e85597313e8c24da237e16
Alfonso <alfonsojaen200533@gmail.com>
2025-05-09 13:29:16 +0200

    Crear metodo y añadir tokend para bizum

---
+++ ViajesAPI/Models/BizumPaymentRequest.cs
@@ -0,0 +1,10 @@
+public class BizumPaymentRequest
+{
+    public string Data { get; set; }
+    public string Order { get; set; }
+    public string Importe { get; set; }
+    public string UrlPageOk { get; set; }
+    public string UrlPageKO { get; set; }
+    public string RedsysNotificationApi { get; set; }
+    public int LanguageTpv { get; set; }
+}
